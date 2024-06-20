8a8a43cf8f318aeb0cbeb7a44b8e9651c69c1495
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-06-20 17:38:10 +0300

    add primary and secondary color

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      primary:colors.orange,
+      secondary:colors.stone,
+
+    },
     extend: {},
   },
   plugins: [],
