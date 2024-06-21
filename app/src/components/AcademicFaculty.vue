63c446dcb150a51012c0dc8e773aec7bce26e346
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-06-21 23:49:44 +0300

    message

---
+++ app/src/components/AcademicFaculty.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="mx-auto max-w-7xl">
+    <div class="grid grid-cols-1 md:grid-cols-12 gap-10">
+      <div class="rounded-2xl p-6 border-2 border-gray-200 md:col-span-8">
+        <h1 class="mb-3 font-bold text-gray-900 text-xl md:text-2xl text-center">
+          Faculty Staff
+        </h1>
+        <!-- Image -->
+        <div class="h-80 w-full sm:w-4/5 bg-gray-200 mx-auto rounded-3xl"></div>
+        <p class="text-lg text-gray-900 text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, unde.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          voluptate excepturi aut cum ullam, doloribus optio enim dolorem
+          fugiat, ad, corporis quibusdam! Nihil quae aliquid dolore nostrum
+          sequi rem unde.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, unde.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          voluptate excepturi aut cum ullam, doloribus optio enim dolorem
+          fugiat, ad, corporis quibusdam! Nihil quae aliquid dolore nostrum
+          sequi rem unde.
+          
+        </p>
+      </div>
+      <div class="rounded-2xl p-4 border-2 border-gray-200 md:col-span-4 min-h-content">
+        <h1 class="mb-3 font-bold text-gray-900 text-xl md:text-2xl">
+          Tech Hub
+        </h1>
+        <p class="text-lg text-gray-900">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, unde.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, unde.
+          
+        </p>
+      </div>
+    </div>
+    
+  </section>
+</template>
+
+<script setup></script>
