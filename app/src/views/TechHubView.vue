aaf88bef6322daa1809a97e9eb49e02e32756bcf
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-06-22 02:20:07 +0300

    message

--- app/src/views/TechHubView.vue
+++ app/src/views/TechHubView.vue
@@ -1,11 +1,13 @@
 <template>
     <HubHeroSection />
     <TechhubGallery />
+    <TechhubOurPartners />
 </template>
 
 <script setup>
 import HubHeroSection from "../components/HubHeroSection.vue";
 import TechhubGallery from "../components/TechhubGallery.vue";
+import TechhubOurPartners from "../components/TechhubOurPartners.vue";
 
 </script>
 
