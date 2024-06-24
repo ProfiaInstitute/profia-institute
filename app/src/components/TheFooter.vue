4f860f794946e01f5287dccb1903ff130a99b524
ian723 <ndegwaian241@gmail.com>
2024-06-24 13:31:31 +0300

    message

--- app/src/components/TheFooter.vue
+++ app/src/components/TheFooter.vue
@@ -1,7 +1,144 @@
 <template>
-    <div>Footer</div>
-  </template>
-  
-  <script setup></script>
-  
-  <style lang="scss" scoped></style>
+  <footer class="bg-white">
+    <div class="container px-6 py-16 mx-auto">
+      <hr class="my-6 border-gray-200 md:my-10" />
+      <div
+        class="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-between"
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZYNnmOkPezPCwn0HNtGEpeL1gD2hyzz8TFw&s"
+          alt="Company Logo"
+          class="h-12"
+        />
+      </div>
+      <div
+        class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+      >
+        <div>
+          <div class="flex flex-col items-center mt-5 space-y-2">
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >About us</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Contact</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Events</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Tech hub</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Enquiries</a
+            >
+          </div>
+        </div>
+
+        <div>
+          <div class="flex flex-col items-center mt-5 space-y-2">
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Academics</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >School of Business</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >School of Hospitality</a
+            >
+          </div>
+        </div>
+
+        <div>
+          <div class="flex flex-col items-center mt-5 space-y-2">
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Administration</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Staff</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Students</a
+            >
+            <a
+              href="#"
+              class="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+              >Admission</a
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-white rounded-lg m-4">
+        <div
+          class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"
+        >
+          <span class="text-sm text-gray-500 sm:text-center"
+            >© 2024.Profia Institute. All Rights Reserved
+          </span>
+          <ul
+            class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0"
+          >
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center w-12 h-12 rounded-full text-gray-900 bg-pink-300 border-3 border-black hover:bg-gray-900 hover:text-white me-4 md:me-6"
+                >T</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center w-12 h-12 rounded-full text-gray-900 bg-pink-300 border-3 border-black hover:bg-gray-900 hover:text-white me-4 md:me-6"
+                >W</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center w-12 h-12 rounded-full text-gray-900 bg-pink-300 border-3 border-black hover:bg-gray-900 hover:text-white me-4 md:me-6"
+                >I</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center w-12 h-12 rounded-full text-gray-900 bg-pink-300 border-3 border-black hover:bg-gray-900 hover:text-white me-4 md:me-6"
+                >F</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center w-12 h-12 rounded-full text-gray-900 bg-pink-300 border-3 border-black hover:bg-gray-900 hover:text-white"
+                >X</a
+              >
+            </li>
+          </ul>
+        </div>
+        <hr class="my-6 border-gray-200 md:my-10" />
+      </div>
+    </div>
+  </footer>
+</template>
