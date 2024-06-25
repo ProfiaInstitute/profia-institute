ee024c5a20a6018cdbd990208cf85294eecbc37a
billClinton048 <clintonkaranja46@gmail.com>
2024-06-25 05:27:06 +0300

    message

--- app/src/components/HubHeroSection.vue
+++ app/src/components/HubHeroSection.vue
@@ -1,29 +1,34 @@
 <template>
-  <section class="pb-10">
+  <section class="">
     <!-- container div -->
-    <div class="flex flex-col-reverse w-full h-[50vh] mx-auto relative lg:h-[80vh] lg:max-w-7xl">
+    <div
+      class="flex flex-col-reverse w-full h-[56vh] mx-auto relative sm:h-[52vh] lg:h-[72vh] lg:max-w-7xl"
+    >
       <!-- div that holds the image  -->
-      <div class="rounded-3xl h-[30vh] p-2 lg:h-[70vh] lg:w-11/12">
+      <div class="rounded-3xl h-[35vh] px-4 sm:h-[32vh] lg:h-[70vh] lg:w-11/12">
         <img
-          class="h-full w-full rounded-3xl shadow-2xl shadow-blue-200"
+          class="h-full w-full rounded-3xl shadow-2xl lg:shadow-blue-200"
           src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"
           alt=""
         />
       </div>
       <!-- Div that hold -->
       <div
-        class="p-2 bg-slate-50 bottom-1 rounded-xl right-3 w-full h-[20vh] lg:w-1/2 lg:absolute shadow-2xl lg:h-[30vh] shadow-blue-200"
+        class="p-4 bg-slate-50 rounded-xl -bottom-5 right-3 w-full lg:w-1/2 lg:absolute lg:shadow-2xl lg:h-[25vh] shadow-blue-200"
       >
         <h1
-          class="text-2xl text-yellow-800 sm:p-2 text-center tracking-tighter font-bold lg:text-5xl"
+          class="text-3xl text-yellow-800 font-serif text-center tracking-tighter sm:text-5xl font-bold lg:text-5xl"
         >
           Profia Tech Hub
         </h1>
-        <p class="text-justify lg:text-xl sm:p-2">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi aliquam
-          voluptate perferendis velit ipsum reprehenderit asperiores. Temporibus
-          placeat nihil quo quae tempora, consequuntur omnis perferendis! Vitae
-          quidem provident voluptates tempore.
+        <p
+          class="text-justify text-xl tracking-tighter font-serif sm:text-2xl lg:text-xl p-1"
+        >
+          Profia Institute's Tech Hub is central to our focus on innovation and
+          technology. We explore AI, Data Categorization, 3D & 2D Annotation,
+          Training Data for Autonomous Driving, NLP, and Video Annotation. Our
+          goal is to advance technology and inspire innovation among students
+          and faculty.
         </p>
       </div>
     </div>
