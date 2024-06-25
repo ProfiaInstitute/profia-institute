8400fc03616f3a13dd6444f6c876c94bc322872e
billClinton048 <clintonkaranja46@gmail.com>
2024-06-25 08:17:14 +0300

    message

--- app/src/components/HubHeroSection.vue
+++ app/src/components/HubHeroSection.vue
@@ -8,7 +8,7 @@
       <div class="rounded-3xl h-[35vh] px-4 sm:h-[32vh] lg:h-[70vh] lg:w-11/12">
         <img
           class="h-full w-full rounded-3xl shadow-2xl lg:shadow-blue-200"
-          src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"
+          src="https://plus.unsplash.com/premium_photo-1682129746418-b86f768e4c5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHRlY2glMjBodWIlMjBpbWFnZXMlMjBmb3IlMjBhZnJpY2Fuc3xlbnwwfHwwfHx8MA%3D%3D"
           alt=""
         />
       </div>
@@ -17,12 +17,12 @@
         class="p-4 bg-slate-50 rounded-xl -bottom-5 right-3 w-full lg:w-1/2 lg:absolute lg:shadow-2xl lg:h-[25vh] shadow-blue-200"
       >
         <h1
-          class="text-3xl text-yellow-800 font-serif text-center tracking-tighter sm:text-5xl font-bold lg:text-5xl"
+          class="text-3xl text-yellow-800 font-serif text-center tracking-tighter sm:text-4xl font-bold "
         >
           Profia Tech Hub
         </h1>
         <p
-          class="text-justify text-xl tracking-tighter font-serif sm:text-2xl lg:text-xl p-1"
+          class="text-justify text-xl tracking-tighter font-serif sm:text-2xl  p-1"
         >
           Profia Institute's Tech Hub is central to our focus on innovation and
           technology. We explore AI, Data Categorization, 3D & 2D Annotation,
