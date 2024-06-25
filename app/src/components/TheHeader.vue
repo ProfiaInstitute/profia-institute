274304f7c95a121fa7a76dd03182f55955fd6d9a
ian723 <ndegwaian241@gmail.com>
2024-06-25 12:50:39 +0300

    message

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -3,12 +3,12 @@
     <!-- Header section -->
     <header
       :class="{
-        'h-14 lg:h-36 fixed w-full top-0 inset-0 z-20 bg-blue-500': isHomePage,
-        'h-14 lg:h-24 fixed w-full top-0 inset-0 z-20 bg-blue-500': !isHomePage,
+        'h-14 lg:h-36 fixed w-full top-0 inset-0 z-20 bg-gray-400': isHomePage,
+        'h-14 lg:h-24 fixed w-full top-0 inset-0 z-20 bg-gray-400': !isHomePage,
       }"
     >
       <!-- Navigation specific to homepage -->
-      <section v-if="isHomePage" class="bg-yellow-500 py-4 lg">
+      <section v-if="isHomePage" class="bg-yellow-700 py-4 lg">
         <div class="flex justify-end items-center list-none px-5 space-x-10">
           <li>
             <router-link to="/techhub" class="hover:text-gray-400"
@@ -43,7 +43,7 @@
         <!-- Navigation menu -->
         <nav
           :class="{ hidden: close2, flex: !close2 }"
-          class="bg-blue-500 lg lg:static lg:flex lg:items-center font-mono flex-1 justify-center absolute top-14 lg:top-0 left-0 w-full lg:w-auto"
+          class="bg-gray-400 lg lg:static lg:flex lg:items-center font-mono flex-1 justify-center absolute top-14 lg:top-0 left-0 w-full lg:w-auto"
         >
           <!-- Navigation items -->
           <ul
@@ -51,13 +51,13 @@
           >
             <!-- Navigation items -->
             <li>
-              <router-link to="/" class="hover:text-gray-400">Home</router-link>
+              <router-link to="/" class="hover:text-white">Home</router-link>
             </li>
             <!-- Academics dropdown -->
             <li class="relative group">
               <router-link
                 to="academics"
-                class="hover:text-gray-400 flex items-center"
+                class="hover:text-white flex items-center"
               >
                 <span>Academics</span>
                 <span class="ml-1">&#9662;</span>
@@ -72,17 +72,17 @@
                     <h3 class="font-semibold">School of Business</h3>
                     <ul class="ml-4">
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 1</router-link
                         >
                       </li>
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 2</router-link
                         >
                       </li>
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 3</router-link
                         >
                       </li>
@@ -93,17 +93,17 @@
                     <h3 class="font-semibold">School of ICT</h3>
                     <ul class="ml-4">
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 1</router-link
                         >
                       </li>
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 2</router-link
                         >
                       </li>
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 3</router-link
                         >
                       </li>
@@ -114,17 +114,17 @@
                     <h3 class="font-semibold">School of Hospitality</h3>
                     <ul class="ml-4">
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 1</router-link
                         >
                       </li>
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 2</router-link
                         >
                       </li>
                       <li>
-                        <router-link to="#" class="block py-1 hover:bg-gray-100"
+                        <router-link to="#" class="block py-1 hover:bg-white"
                           >link 3</router-link
                         >
                       </li>
@@ -135,22 +135,22 @@
             </li>
             <!-- Other navigation items -->
             <li>
-              <router-link to="aboutus" class="hover:text-gray-400"
+              <router-link to="aboutus" class="hover:text-white"
                 >About Us</router-link
               >
             </li>
             <li>
-              <router-link to="news" class="hover:text-gray-400"
+              <router-link to="news" class="hover:text-white"
                 >News & updates</router-link
               >
             </li>
             <li>
-              <router-link to="contact" class="hover:text-gray-400"
+              <router-link to="contact" class="hover:text-white"
                 >Contact Us</router-link
               >
             </li>
             <li>
-              <router-link to="#" class="hover:text-gray-400 relative"
+              <router-link to="#" class="hover:text-white relative"
                 >Admission
                 <img
                   src="@/assets/Search.png"
@@ -188,7 +188,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-/* Add any necessary styling here */
-</style>
