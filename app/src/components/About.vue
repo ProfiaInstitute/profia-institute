1e6d49a79d69e41bccfa5c06929867c1190af472
meccah <georgekimiti79@gmail.com>
2024-06-26 10:59:24 +0300

    message

--- app/src/components/About.vue
+++ app/src/components/About.vue
@@ -1,21 +1,53 @@
 <template>
   <section class="max-w-7xl mx-auto">
     <div class="max-w-5xl mx-auto">
-    <nav class="text-xl text-gray-600 py-10 flex justify-end items-center mr-40">
-      Our Mission &rarr;<br />
-      Our Vision &rarr;<br />
-      The Core Values&rarr;
-    </nav>
-    <div
-        class="relative bg-blue-500 h-16 flex justify-end items-center text-white mx-auto"
+      <nav
+        class="text-2xl text-gray-600 py-10 flex justify-end items-center mr-40"
+      >
+        Our Mission &rarr;<br />
+        Our Vision &rarr;<br />
+        The Core Values&rarr;
+      </nav>
+      <div
+        class="relative bg-yellow-700 h-16 flex justify-end items-center text-white mx-auto"
       >
         <span class="mr-48">Who we are</span>
         <div class="absolute right-[600px] w-64 h-64 bg-gray-300">
           <img src="" alt="Image Description" class="w-full" />
         </div>
       </div>
+      <div class="space-y-24 pt-44">
+        <div
+          class="relative p-8 bg-slate-100 rounded-lg shadow-lg lg:ml-48"
+        >
+          <div
+            class="absolute -top-8 left-5  bg-gray-400 px-8 py-4 rounded-xl"
+          >
+            <h2 class="text-3xl">Our Mission</h2>
+          </div>
+          <p class="py-5 text-2xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam.
+          </p>
+        </div>
+
+        <div
+          class="relative p-8 bg-slate-100 rounded-lg shadow-lg lg:mr-48"
+        >
+          <div
+            class="absolute -top-8 right-5  bg-gray-400 px-8 py-4 rounded-md shadow-sm"
+          >
+            <h1 class="text-3xl">Our Vision</h1>
+          </div>
+          <p class="py-5 text-2xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
+
   <div class="justify-center">
     <div class="relative">
       <!-- <div
@@ -37,11 +69,11 @@
         </div>
       </div>
     </div>
-    <div
+    <!-- <div
       class="flex flex-col space-y-8 space-x-4 md:space-x-24 justify-center mt-12 md:ml-16 lg:ml-52"
     >
       <div
-        class="relative w-full max-w-xl p-8 bg-gray-300 rounded-md shadow-md md:justify-center lg:ml-96"
+        class="relative w-full max-w-2xl p-8 bg-gray-300 rounded-md shadow-md md:justify-center lg:ml-96"
       >
         <div
           class="absolute top-0 left-2 transform -translate-y-1/2 bg-gray-400 px-8 py-4 rounded-md shadow-sm"
@@ -67,7 +99,7 @@
           odio. Praesent libero. Sed cursus ante dapibus diam.
         </p>
       </div>
-    </div>
+    </div> -->
     <div
       class="bg-gray-200 rounded-lg p-6 relative w-full max-w-xl mx-auto mt-12"
     >
