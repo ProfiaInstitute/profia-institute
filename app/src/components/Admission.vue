dfe009d6165eade06b177450b8d2812a1b2f3bf5
meccah <georgekimiti79@gmail.com>
2024-06-28 01:09:53 +0300

    message

--- app/src/components/Admission.vue
+++ app/src/components/Admission.vue
@@ -5,7 +5,7 @@
         <div class="max-w-5xl mx-auto flex justify-between items-center py-20">
           <h1 class="text-8xl text-primary-900">Admission</h1>
           <Button
-            class="border-[1px] border-black bg-secondary-600 px-5 py-3 rounded-xl hover:bg-black hover:rounded-full hover:text-white"
+            class="border-[1px] border-black bg-secondary-600 px-5 py-3 rounded-xl transition duration-700 ease-in-out hover:bg-black hover:rounded-full hover:text-white"
             >Click to Apply</Button
           >
         </div>
@@ -48,7 +48,7 @@
                 Intermediate education is a level of education that bridges the
                 gap between primary and higher education.
               </p>
-              <div class="absolute -left-40 top-48">
+              <!-- <div class="absolute -left-40 top-48">
                 <div class="flex">
                   <img
                     src="../assets/images/techhub-1.avif"
@@ -75,7 +75,7 @@
                     class="w-36 h-28"
                   />
                 </div>
-              </div>
+              </div> -->
             </div>
             <div class="py-5 w-72 px-5 space-y-4 border border-black">
               <h1 class="text-2xl font-semibold text-center">Certificate</h1>
@@ -87,7 +87,7 @@
           </div>
           <div class="py-4 flex justify-center items-center">
             <button
-              class="border-[1px] border-black bg-secondary-500 px-5 py-3 text-3xl rounded-xl hover:bg-black hover:rounded-full hover:text-white"
+              class="border-[1px] border-black bg-secondary-500 px-5 py-3 text-3xl rounded-xl traansition duration-700 ease-in-out hover:bg-black hover:rounded-full hover:text-white"
             >
               Apply Now
             </button>
