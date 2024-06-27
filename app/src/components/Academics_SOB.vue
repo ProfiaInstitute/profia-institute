de39f5669eed0a545ddfcedfd0e60a8e77b21764
meccah <georgekimiti79@gmail.com>
2024-06-27 08:14:22 +0300

    message

--- app/src/components/Academics_SOB.vue
+++ app/src/components/Academics_SOB.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="max-w-7xl mx-auto py-5 mt-24">
     <div class="">
-      <h1 class="flex justify-center items-center text-4xl text-orange-900 font-bold mb-4">ACADEMICS</h1>
+      <h1
+        class="flex justify-center items-center text-4xl text-orange-900 font-bold mb-4"
+      >
+        ACADEMICS
+      </h1>
       <div class="flex flex-row justify-center items-center">
         <div>
           <h1 class="text-2xl font-semibold">Lorem ipsum dolor!!</h1>
@@ -31,25 +35,30 @@
             class="mt-4 text-center border rounded-xl w-auto h-auto space-y-5 p"
           >
             <h1 class="text-2xl font-bold py-5">Message From The Principal</h1>
-            <p class="text-lg ">
-              Welcome to our esteemed institution! At our school, we are dedicated to providing high-quality education and 
-              practical training across various fields. Whether you are pursuing a career in ICT, Business, 
-              Engineering, Hospitality, or Beauty/Cosmetology, our programs are designed to equip 
-              you with the skills and knowledge needed to succeed. Our faculty are committed to your growth and excellence, 
-              ensuring you are prepared for the dynamic and ever-evolving job market. 
-              Join us and embark on a journey towards a bright and fulfilling future.
+            <p class="text-lg">
+              Welcome to our esteemed institution! At our school, we are
+              dedicated to providing high-quality education and practical
+              training across various fields. Whether you are pursuing a career
+              in ICT, Business, Engineering, Hospitality, or Beauty/Cosmetology,
+              our programs are designed to equip you with the skills and
+              knowledge needed to succeed. Our faculty are committed to your
+              growth and excellence, ensuring you are prepared for the dynamic
+              and ever-evolving job market. Join us and embark on a journey
+              towards a bright and fulfilling future.
             </p>
             <p>
-              Sincerely,<br>
-              Mr/Mrs<br>
+              Sincerely,<br />
+              Mr/Mrs<br />
               Profia Institute
             </p>
           </div>
         </div>
-        <div class="col-span-4 mx-auto space-y-5 my-4 ">
-          <div class="border border-black w-[400px] h-[200px] rounded-xl px-5">
+        <div class="col-span-4 mx-auto space-y-5 my-4">
+      
+        <CallToActionSOB/>  
+          <!-- <div class="border border-black w-[400px] h-[200px] rounded-xl px-5">
             <h1 class="text-xl font-semibold">Lorem ipsum dolor!!</h1>
-            <h2 class="text-lg ">
+            <h2 class="text-lg">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
               eligendi explicabo, repudiandae blanditiis quam magnam
             </h2>
@@ -58,36 +67,39 @@
               type="button"
               value="APPLY HERE"
             />
-          </div>
-          <div class="border border-black rounded-lg w-[400px] h-[600px] px-5 space-y-5">
+          </div> -->
+          <div
+            class="border border-black rounded-lg w-[400px] h-[600px] px-5 space-y-5"
+          >
             <div class="py-5">
               <h1 class="text-xl font-semibold">School of Business</h1>
               <p class="text-lg">
-                Elevate your passion for beauty with our top-notch cosmetology programs.<br />
-                Turn your creativity into a rewarding career in the beauty industry.
+                Elevate your passion for beauty with our top-notch cosmetology
+                programs.<br />
+                Turn your creativity into a rewarding career in the beauty
+                industry.
               </p>
             </div>
             <div class="">
               <h1 class="text-xl font-bold">School of Engineering</h1>
               <p class="text-lg">
                 Shape the future with a engineering course.<br />
-                Innovate and engineer solutions that make a difference by tackling
-                real-world challenges
+                Innovate and engineer solutions that make a difference by
+                tackling real-world challenges
               </p>
             </div>
           </div>
-
-
           <div
-        class="rounded-2xl p-4 border-2 border-gray-200 md:col-span-4 md:h-1/2"
-      >
-        <h1 class="mb-3 font-bold text-gray-900 text-xl md:text-2xl">
-          Tech Hub
-        </h1>
-        <p class="text-lg text-gray-900">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, unde.
-        </p>
-      </div>
+            class="rounded-2xl p-4 border-2 border-gray-200 md:col-span-4 md:h-1/2"
+          >
+            <h1 class="mb-3 font-bold text-gray-900 text-xl md:text-2xl">
+              Tech Hub
+            </h1>
+            <p class="text-lg text-gray-900">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+              unde.
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -95,7 +107,7 @@
 </template>
 <style scoped></style>
 <script setup>
-  import CallToActionAcademic from './CallToActionAcademic.vue';
-import CallToActionProfia from './CallToActionProfia.vue';
-  import CallToActionSOB from './CallToActionSOB.vue';
+import CallToActionAcademic from "./CallToActionAcademic.vue";
+import CallToActionProfia from "./CallToActionProfia.vue";
+import CallToActionSOB from "./CallToActionSOB.vue";
 </script>
