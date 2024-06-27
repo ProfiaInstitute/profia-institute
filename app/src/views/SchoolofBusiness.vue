dad6f5f7512760e144718d620d98404c2161ce29
ian723 <ndegwaian241@gmail.com>
2024-06-27 13:08:21 +0300

    message

--- app/src/views/SchoolofBusiness.vue
+++ app/src/views/SchoolofBusiness.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <Business />
+    <Whatyouearn/>
     <Faqs />
   </div>
 </template>
 
 <script setup>
 import Business from "../components/Business.vue";
+import Whatyouearn from "../components/Whatyouearn.vue";
 import Faqs from "../components/Faqs.vue";
 </script>
