d406fc207eacae57f6776d4320e9deefe489d183
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-06-27 18:17:23 +0300

    message

--- app/src/components/CallToActionSOB.vue
+++ app/src/components/CallToActionSOB.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="p-5 rounded-2xl m-3 border-2 border-black">
     <div class="bg-orange-100 px-8 py-10 text-left rounded-2xl">
-      <h4 class="text-3xl">Profia Institute of Profesion</h4>
+      <h4 class="text-3xl">Profia Institute of Professional Studies</h4>
       <p class="text-xl">offers you a challenge that is industry relevant</p>
       <div class="flex justify-center mt-2">
         <button
-          class="flex items-center justify-center border-2 border-black py-2 px-4 rounded-lg mt-2"
+          class="flex items-center justify-center border-2 border-black py-2 px-4 rounded-xl mt-2 hover:bg-primary-800 hover:text-white"
           type="button"
         >
           Apply for the Course
