840444508c6baa46febdc6503e1a892c3290dbf3
ian723 <ndegwaian241@gmail.com>
2024-06-27 12:23:33 +0300

    message

--- app/src/views/index.js
+++ app/src/views/index.js
@@ -1,7 +1,7 @@
 import HomeView from "./HomeView.vue";
 import TechHubView from "./TechHubView.vue";
 import Academics from "./Academics.vue";
-import SchoolofBussines from "./SchoolofBussines.vue";
+import SchoolofBusiness from "./SchoolofBusiness.vue";
 import NewsView from "./NewsView.vue";
 import AboutusView from "./AboutusView.vue";
 import ContactView from "./ContactView.vue";
@@ -10,7 +10,7 @@ export {
   HomeView,
   TechHubView,
   Academics,
-  SchoolofBussines,
+  SchoolofBusiness,
   NewsView,
   AboutusView,
   ContactView
