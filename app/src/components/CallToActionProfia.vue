71d65b83a2141fcfa1b5a5c6d19fc1f2aeb96568
meccah <georgekimiti79@gmail.com>
2024-06-28 08:38:49 +0300

    message

--- app/src/components/CallToActionProfia.vue
+++ app/src/components/CallToActionProfia.vue
@@ -1,14 +1,22 @@
 <template>
-  <section class="bg-gradient-to-r from-white from-45% via-gray-500  to-yellow-900 ">
-    <div
-      class="w-full h-28 py-5 flex justify-between "
-    >
+  <section class="bg-gradient-to-r from-gray-100 from-45% to-yellow-900">
+    <div class="w-full h-28 py-5 flex justify-between">
       <div class="px-4">
         <h3 class="text-4xl">Profia Institute of Professionals</h3>
         <p>with quality comes good business</p>
       </div>
       <div class="text-2xl px-24 justify-center items-center text-center">
-        <button class="border-[2px] border-black px-4 py-3">Apply Now</button>
+        <!-- <router-link
+          to="https://docs.google.com/forms/d/e/1FAIpQLScMpwjvMblufTl-aLDPacOiPc1eD2OtH86z9kV6GaEU6TdM0w/viewform"
+          ><button class="border-[2px] border-black px-4 py-3">
+            Apply Now
+          </button></router-link
+        > -->
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScMpwjvMblufTl-aLDPacOiPc1eD2OtH86z9kV6GaEU6TdM0w/viewform"
+        > <button class="border-[2px] border-black px-4 py-3">
+            Apply Now
+          </button></a>
       </div>
     </div>
   </section>
