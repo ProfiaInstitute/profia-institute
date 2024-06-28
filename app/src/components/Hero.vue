8135c593ee24c8c31a44899120cfe367e09616c2
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-06-28 13:50:57 +0300

    message

---
+++ app/src/components/Hero.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="!flex h-[90vh] w-full items-center justify-between px-10">
+      <img
+        src="https://bucket.material-tailwind.com/magic-ai/3f24080b2fa162ec33033ec532f2d727ad79b2ca73ccb0f0ab67f99d2e56c096.jpg"
+        alt="bg-img"
+        class="absolute inset-0 ml-auto h-full rounded-bl-[100px] object-cover object-center"
+      />
+      <div class="container mx-auto lg:mt-0">
+        <div class="grid grid-cols-12 text-center lg:text-left">
+          <div
+            class="relative flex flex-col bg-clip-border text-gray-700 px-6 py-10 border border-white shadow-lg col-span-full rounded-xl bg-white/90 shadow-black/10 backdrop-blur-sm backdrop-saturate-200 xl:col-span-4"
+          >
+            <h1
+              class="block antialiased tracking-normal font-sans font-semibold text-blue-gray-900 text-3xl !leading-snug lg:text-5xl"
+            >
+              Profia Institute <br />of technologies
+            </h1>
+            <p
+              class="block antialiased font-sans text-xl font-normal leading-relaxed text-inherit mb-10 mt-6 !text-gray-900"
+            >
+              First business college of choice
+            </p>
+            <div class="flex justify-center gap-4 mb-8 lg:justify-start">
+              <button
+                class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                type="button"
+              >
+                contact us</button
+              ><button
+                class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg border border-gray-900 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85]"
+                type="button"
+              >
+                read more
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center mt-16 space-x-4">
+      <button
+        class="bg-blue-300 hover:bg-blue-400 text-black font-bold py-4 px-8 rounded"
+      >
+        Courses
+      </button>
+      <button
+        class="bg-blue-300 hover:bg-blue-400 text-black font-bold py-4 px-8 rounded"
+      >
+        About
+      </button>
+      <button
+        class="bg-blue-300 hover:bg-blue-400 text-black font-bold py-4 px-8 rounded"
+      >
+        Campus Life
+      </button>
+      <button
+        class="bg-blue-300 hover:bg-blue-400 text-black font-bold py-4 px-8 rounded"
+      >
+        Apply Today
+      </button>
+    </div>
+   
+
+    
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="px-4 sm:px-8">
+    <Hero/>
+    <AdverHero/>
     <HomeCards />
+    <UpdateHero/>
+    <PatnerHero/>
+    
   </div>
 </template>
 
 <script setup>
+import AdverHero from "../components/AdverHero.vue";
+import PatnerHero from "../components/PatnerHero.vue";
+import Hero from "../components/Hero.vue";
 import HomeCards from "../components/HomeCards.vue";
+import UpdateHero from "../components/UpdateHero.vue";
 </script>
 
 <style lang="scss" scoped></style>
