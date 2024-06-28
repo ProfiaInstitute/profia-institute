de841b596c1fd2c6faa5cd3c59183cda2e5b2e39
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-06-28 11:48:42 +0300

    message

--- app/src/components/About.vue
+++ app/src/components/About.vue
@@ -1,47 +1,70 @@
-
-<template><div class="justify-center">
+<template>
+  <div class="justify-center">
     <div class="relative">
-        <div class="fixed top-52 left-1/2 transform -translate-x-1/2 bg-blue-500 h-16 w-5/6 flex justify-center items-center text-white">
-            <span class="">Who we are</span>
+      <div
+        class="fixed top-52 left-1/2 transform -translate-x-1/2 bg-blue-500 h-16 w-5/6 flex justify-center items-center text-white"
+      >
+        <span class="">Who we are</span>
+      </div>
+      <div
+        class="container mx-auto flex flex-col md:flex-row items-center justify-start relative z-10 pt-16"
+      >
+        <div class="w-64 h-64 bg-gray-300">
+          <img src="" alt="Image Description" class="w-full" />
         </div>
-        <div class="container mx-auto flex flex-col md:flex-row items-center justify-start relative z-10 pt-16">
-            <div class="w-64 h-64 bg-gray-300">
-                <img src="" alt="Image Description" class="w-full" />
-            </div>
-            <div class="flex flex-col md:mb-56 md:ml-72 space-y-2">
-                <nav class="text-xl text-gray-600 mt-4 md:mt-0 md:ml-8">
-                    Our Mission &rarr;<br />
-                    Our Vision &rarr;<br />The Core Values&rarr;
-                </nav>
-            </div>
+        <div class="flex flex-col md:mb-56 md:ml-72 space-y-2">
+          <nav class="text-xl text-gray-600 mt-4 md:mt-0 md:ml-8">
+            Our Mission &rarr;<br />
+            Our Vision &rarr;<br />The Core Values&rarr;
+          </nav>
         </div>
+      </div>
     </div>
-    <div class="flex flex-col space-y-8 space-x-4 md:space-x-24 justify-center mt-12 md:ml-16 lg:ml-52">
-        <div class="relative w-full max-w-xl p-8 bg-gray-300 rounded-md shadow-md md:justify-center lg:ml-96">
-            <div class="absolute top-0 left-2 transform -translate-y-1/2 bg-gray-400 px-8 py-4 rounded-md shadow-sm">
-                Our Mission
-            </div>
-            <p class="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
-            </p>
+    <div
+      class="flex flex-col space-y-8 space-x-4 md:space-x-24 justify-center mt-12 md:ml-16 lg:ml-52"
+    >
+      <div
+        class="relative w-full max-w-xl p-8 bg-gray-300 rounded-md shadow-md md:justify-center lg:ml-96"
+      >
+        <div
+          class="absolute top-0 left-2 transform -translate-y-1/2 bg-gray-400 px-8 py-4 rounded-md shadow-sm"
+        >
+          Our Mission
         </div>
-
-        <div class="relative w-full max-w-xl p-8 bg-gray-300 rounded-md shadow-md lg:mr-48">
-            <div class="absolute top-0 right-2 transform -translate-y-1/2 bg-gray-400 px-8 py-4 rounded-md shadow-sm">
-                Our Vision
-            </div>
-            <p class="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
-            </p>
-        </div>
-    </div>
-    <div class="bg-gray-200 rounded-lg p-6 relative w-full max-w-xl mx-auto mt-12">
-        <div class="absolute -top-5 left-1/2 transform -translate-x-1/2 bg-gray-400 text-white px-4 py-2 rounded-md">
-            The Core Values
-        </div>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, delectus ab tenetur nam provident tempora earum commodi illo. Et eveniet hic rerum facilis. Labore facilis obcaecati repellat eius, quo adipisci voluptatum cumque deserunt at odio. Odit fugiat sit expedita veritatis.
+        <p class="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam.
         </p>
-    </div>
-</div></template>
+      </div>
 
+      <div
+        class="relative w-full max-w-xl p-8 bg-gray-300 rounded-md shadow-md lg:mr-48"
+      >
+        <div
+          class="absolute top-0 right-2 transform -translate-y-1/2 bg-gray-400 px-8 py-4 rounded-md shadow-sm"
+        >
+          Our Vision
+        </div>
+        <p class="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam.
+        </p>
+      </div>
+    </div>
+    <div
+      class="bg-gray-200 rounded-lg p-6 relative w-full max-w-xl mx-auto mt-12"
+    >
+      <div
+        class="absolute -top-5 left-1/2 transform -translate-x-1/2 bg-gray-400 text-white px-4 py-2 rounded-md"
+      >
+        The Core Values
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+        delectus ab tenetur nam provident tempora earum commodi illo. Et eveniet
+        hic rerum facilis. Labore facilis obcaecati repellat eius, quo adipisci
+        voluptatum cumque deserunt at odio. Odit fugiat sit expedita veritatis.
+      </p>
+    </div>
+  </div>
+</template>
