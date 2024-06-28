7760548fc697c0dd11b4264311958c14ed6a1032
meccah <georgekimiti79@gmail.com>
2024-06-28 10:34:19 +0300

    message

--- app/src/components/Academics_SOB.vue
+++ app/src/components/Academics_SOB.vue
@@ -54,7 +54,7 @@
             </p>
           </div>
         </div>
-        <div class="col-span-4 mx-auto space-y-5 my-4 px-5">
+        <div class="fixed ml-[1000px] max-w-md  col-span-4 mx-auto space-y-5 my-4 px-5">
           <CallToActionSOB />
           <!-- <div class="border border-black w-[400px] h-[200px] rounded-xl px-5">
             <h1 class="text-xl font-semibold">Lorem ipsum dolor!!</h1>
@@ -129,6 +129,69 @@
             </p> -->
           </div>
         </div>
+      </div>
+    </div>
+  </section>
+  <section class="mx-auto max-w-7xl">
+    <div class="grid grid-cols-1 md:grid-cols-12 gap-10">
+      <div class="rounded-2xl p-6 border-2 border-gray-200 md:col-span-8">
+        <h1
+          class="mb-3 font-bold text-gray-900 text-2xl md:text-3xl text-center"
+        >
+          Faculty Staff
+        </h1>
+        <img
+          src="@/assets/images/photo-faculty.avif"
+          alt="staff"
+          class="w-full sm:w-4/5 mx-auto my-4 object-cover aspect-square rounded-2xl"
+          loading="lazy"
+        />
+        <p class="text-lg text-gray-900 text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, unde.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          voluptate excepturi aut cum ullam, doloribus optio enim dolorem
+          fugiat, ad, corporis quibusdam! Nihil quae aliquid dolore nostrum
+          Repudiandae voluptate excepturi aut cum ullam, doloribus optio enim
+          dolorem
+        </p>
+      </div>
+      <!-- <div
+        class="rounded-2xl p-4 border-2 border-gray-200 md:col-span-4 md:h-1/2"
+      >
+        <h1 class="mb-3 font-bold text-gray-900 text-xl md:text-2xl">
+          Tech Hub
+        </h1>
+        <p class="text-lg text-gray-900">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, unde.
+        </p>
+      </div> -->
+    </div>
+  </section>
+
+  <section class="mx-auto max-w-7xl">
+    <div class="grid grid-cols-1 md:grid-cols-12 gap-10 py-12">
+      <div class="md:col-span-8">
+        <h1 class="mb-5 font-bold text-gray-900 text-2xl md:text-3xl">
+          Programs on offer
+        </h1>
+        <ul class="text-lg text-gray-900 space-y-5">
+
+          <li>&rarr; School of Business</li>
+          <li>&rarr; School of ICT</li>
+          <li>&rarr; School of Hospitality</li>
+          <li>&rarr; School of Beauty/Cosmetology</li>
+          <li>&rarr; School of Engineering</li>
+               
+          
+          <!-- <li>Certified Public Accountant (CPA)</li>
+          <li>Accounting Technician Diploma (ATD)</li>
+          <li>Certificate in Accounting & Management Skills (CAMS)</li>
+          <li>Diploma in Accountancy</li>
+          <li>Business Mathematics</li>
+          <li>Store Keeping</li>
+          <li>Computerized Accounting</li>
+          <li>Secretariat & Front office operations</li> -->
+        </ul>
       </div>
     </div>
   </section>
