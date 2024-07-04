06dfd44791b65afd5ecc7f5c6d812ee37633526a
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-07-04 12:25:50 +0300

    message

--- app/src/components/Faqs.vue
+++ app/src/components/Faqs.vue
@@ -1,48 +1,43 @@
 <template>
-  <div>
-    <section class="py-20 bg-gray-100">
-      <div class="max-w-7xl mx-auto px-4 sm:px-8">
-        <div class="py-8 px-4 mx-auto max-w-7xl text-center lg:py-8 lg:px-0">
-          <h1
-            class="mb-4 text-4xl font-semibold tracking-tight leading-none text-secondary-color md:text-5xl lg:text-6xl"
-          >
-            Frequently Asked Questions
-          </h1>
-          <p
-            class="mb-0 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48"
-          >
-            Exploring Tech: Answers to Your Frequently Asked Questions
-          </p>
-        </div>
-        <div class="max-w-3xl mx-auto">
-          <ul class="mt-0 divide-y-2 rounded-2xl">
-            <li v-for="(faq, index) in faqs" :key="index">
-              <details class="group">
-                <summary
-                  @click="toggleAccordion(index)"
-                  class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
-                >
-                  <span class="font-semibold text-xl py-2">{{
-                    faq.question
-                  }}</span>
-                  <span class="text-2xl">{{ faq.open ? "-" : "+" }}</span>
-                </summary>
-
-                <div
-                  v-show="faq.open"
-                  class="m-4 p-4 rounded-2xl text-gray-500"
-                >
-                  <p>
-                    {{ faq.answer }}
-                  </p>
-                </div>
-              </details>
-            </li>
-          </ul>
-        </div>
+  <section class="py-20 bg-gray-100">
+    <div class="max-w-7xl mx-auto px-4 sm:px-8">
+      <div class="py-8 px-4 mx-auto max-w-7xl text-center lg:py-8 lg:px-0">
+        <h1
+          class="mb-4 text-4xl font-semibold tracking-tight leading-none text-secondary-color md:text-5xl lg:text-6xl"
+        >
+          Frequently Asked Questions
+        </h1>
+        <p
+          class="mb-0 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48"
+        >
+          Exploring Tech: Answers to Your Frequently Asked Questions
+        </p>
       </div>
-    </section>
-  </div>
+      <div class="max-w-3xl mx-auto">
+        <ul class="mt-0 divide-y-2 rounded-2xl">
+          <li v-for="(faq, index) in faqs" :key="index">
+            <details class="group">
+              <summary
+                @click="toggleAccordion(index)"
+                class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
+              >
+                <span class="font-semibold text-xl py-2">{{
+                  faq.question
+                }}</span>
+                <span class="text-2xl">{{ faq.open ? "-" : "+" }}</span>
+              </summary>
+
+              <div v-show="faq.open" class="m-4 p-4 rounded-2xl text-gray-500">
+                <p>
+                  {{ faq.answer }}
+                </p>
+              </div>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
