c5cad15ac4e50e69c16d6c54a572ca96371b1731
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-07-04 14:08:31 +0300

    message

--- app/src/components/Academics_SOB.vue
+++ app/src/components/Academics_SOB.vue
@@ -188,7 +188,7 @@
                   @click="toggleAccordion(index)"
                   class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
                 >
-                  <span class="font-semibold text-xl py-2">{{
+                  <span class="font-semibold text-xl sm:text-2xl py-2">{{
                     faq.school
                   }}</span>
                   <span class="text-2xl">{{ faq.open ? "-" : "+" }}</span>
@@ -196,9 +196,9 @@
 
                 <div
                   v-show="faq.open"
-                  class="m-4 p-4 rounded-2xl text-gray-500"
+                  class=" m-4 p-4 rounded-2xl text-gray-600"
                 >
-                  <p v-for="(program, index) in faq.programs" :key="index">
+                  <p v-for="(program, index) in faq.programs" :key="index" class="text-lg sm:text-xl">
                     {{program }}
                   </p>
                 </div>
@@ -232,48 +232,44 @@ const faqs = ref([
   {
     school: "School of ICT",
     programs:[
-      "Certified Public Accountant (CPA)",
-      "Chartered Secretary (CS)",
-      "Certified Investment and Financial Analyst (CIFA)",
-      "Certified Credit Professional (CCP)",
-      "Accounting Technician Diploma",
-      "Certificate in Accounting and Management Skills"
+      "Computer foundational skills",
+      "Certificate in ICT",
+      "Diploma in ICT",
+      "Graphics Design",
+      "AI & Machine Learning",
+      "Website Development",
+      "Software Development",
+      "Statistical Languages",
+      "Diploma in Data Management and analytics"
     ],
     open: false,
   },
   {
     school: "School of Hospitality",
     programs:[
-      "Certified Public Accountant (CPA)",
-      "Chartered Secretary (CS)",
-      "Certified Investment and Financial Analyst (CIFA)",
-      "Certified Credit Professional (CCP)",
-      "Accounting Technician Diploma",
-      "Certificate in Accounting and Management Skills"
+      "Food and Beverage Production:",
+      "Food and Beverage Services",
+      "Housekeeping",
+      "Baking and Pastry"
     ],
     open: false,
   },
   {
     school: "School of Beauty/Cosmetology",
     programs:[
-      "Certified Public Accountant (CPA)",
-      "Chartered Secretary (CS)",
-      "Certified Investment and Financial Analyst (CIFA)",
-      "Certified Credit Professional (CCP)",
-      "Accounting Technician Diploma",
-      "Certificate in Accounting and Management Skills"
+      "Beauty Therapy",
+      "Hair Dressing",
+      "Nail Technology",
+      "Barbering",
+      "Hot Stone Massage"
     ],
     open: false,
   },
   {
     school: "School of Engineering",
     programs:[
-      "Certified Public Accountant (CPA)",
-      "Chartered Secretary (CS)",
-      "Certified Investment and Financial Analyst (CIFA)",
-      "Certified Credit Professional (CCP)",
-      "Accounting Technician Diploma",
-      "Certificate in Accounting and Management Skills"
+      "Plumbing Program",
+      "Electrical Program"
     ],
     open: false,
   },
