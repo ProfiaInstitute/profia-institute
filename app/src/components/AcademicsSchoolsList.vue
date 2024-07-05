4d05a70ad70ee2fc8a70bb27aeb2eb039bd2a58f
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-07-05 20:11:28 +0300

    rename academics components

---
+++ app/src/components/AcademicsSchoolsList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="border-[2px] border-gray-200 rounded-lg w-auto h-auto px-5 space-y-4 py-3"
+  >
+    <div class="">
+      <router-link to="/sob">
+        <h1 class="text-xl font-semibold underline">School of Business</h1>
+      </router-link>
+    </div>
+    <div class="">
+      <router-link to="/soi">
+        <h1 class="text-xl font-semibold underline">School of ICT</h1>
+      </router-link>
+    </div>
+    <div class="">
+      <router-link to="/soh">
+        <h1 class="text-xl font-semibold underline">School of Hospitality</h1>
+      </router-link>
+    </div>
+    <div class="">
+      <router-link to="/soc">
+        <h1 class="text-xl font-semibold underline">
+          School of Beauty/Cosmetology
+        </h1>
+      </router-link>
+    </div>
+    <div class="">
+      <router-link to="/soe"
+        ><h1 class="text-xl font-semibold underline">School of Engineering</h1>
+      </router-link>
+    </div>
+  </div>
+  <div
+    class="rounded-2xl p-4 border-2 border-gray-200 md:col-span-4 md:min-h-min"
+  >
+    <router-link to="/techhub">
+      <h1 class="mb-3 font-bold text-gray-900 text-xl md:text-2xl">Tech Hub</h1>
+    </router-link>
+  </div>
+  
+</template>
+
+<script setup></script>
--- app/src/views/AcademicsView.vue
+++ app/src/views/AcademicsView.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="px-4 sm:px-8">
-    <Academics_SOB />
-    <!-- <Carousel/> -->
-    <!-- <AcademicsFaculty /> -->
-    <Faqs />
+  <div class="px-4 sm:px-4">
+    <section class="flex flex-col lg:flex-row mx-auto max-w-7xl">
+      <div class="w-full lg:w-8/12">
+        <AcademicsPrincipal />
+        <AcademicsFacultyStaff />
+        <AcademicsSchools />
+      </div>
+      <!-- sticky section -->
+      <div class="my-4 px-5 w-full lg:w-4/12">
+        <div class="sticky top-24 space-y-10">
+          <AcademicsCallToAction />
+          <AcademicsSchoolsList />
+        </div>
+      </div>
+    </section>
+    <AcademicsFaq />
   </div>
 </template>
 
 <script setup>
-import AcademicsFaculty from "../components/AcademicsFaculty.vue";
-import Academics_SOB from "../components/Academics_SOB.vue";
-import Carousel from "../components/Carousel.vue";
-import Faqs from "../components/Faqs.vue";
+import AcademicsCallToAction from "../components/AcademicsCallToAction.vue";
+import AcademicsFacultyStaff from "../components/AcademicsFacultyStaff.vue";
+import AcademicsPrincipal from "../components/AcademicsPrincipal.vue";
+import AcademicsSchools from "../components/AcademicsSchools.vue";
+import AcademicsSchoolsList from "../components/AcademicsSchoolsList.vue";
+import AcademicsFaq from "../components/AcademicsFaq.vue";
 </script>
