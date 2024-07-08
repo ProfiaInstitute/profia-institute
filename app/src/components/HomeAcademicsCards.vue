a395b29806ed93b3f2f47351f9bba010805bbe6a
meccah <georgekimiti79@gmail.com>
2024-07-08 18:23:34 +0300

    message

--- app/src/components/HomeAcademicsCards.vue
+++ app/src/components/HomeAcademicsCards.vue
@@ -3,16 +3,14 @@
     <h2 class="text-4xl font-bold text-center mb-12">Academics</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
       <!-- Card 1 -->
-      <div
-        class="w-full relative rounded-2xl overflow-hidden flex flex-col"
-      >
+      <div class="w-full relative rounded-2xl overflow-hidden flex flex-col">
         <img
           src="https://images.unsplash.com/photo-1716276590903-f7703efa9177?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Night Lights"
           class="w-full h-1/2 object-cover"
         />
         <div
-          class="bg-gray-900 bg-opacity-30 p-6 flex-grow rounded-b-2xl flex flex-col justify-between"
+          class="bg-secondary-color p-6 flex-grow rounded-b-2xl flex flex-col justify-between"
         >
           <div>
             <h1 class="text-2xl font-semibold text-white">
@@ -32,16 +30,14 @@
       </div>
 
       <!-- Card 2 -->
-      <div
-        class="w-full relative rounded-2xl overflow-hidden flex flex-col"
-      >
+      <div class="w-full relative rounded-2xl overflow-hidden flex flex-col">
         <img
           src="https://images.unsplash.com/photo-1716968921433-835ec227f847?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Welcome Aboard"
           class="w-full h-1/2 object-cover"
         />
         <div
-          class="bg-gray-900 bg-opacity-30 p-6 flex-grow rounded-b-2xl flex flex-col justify-between"
+          class="bg-secondary-color p-6 flex-grow rounded-b-2xl flex flex-col justify-between"
         >
           <div>
             <h1 class="text-2xl font-semibold text-white">
@@ -61,16 +57,14 @@
       </div>
 
       <!-- Card 3 -->
-      <div
-        class="w-full relative rounded-2xl overflow-hidden flex flex-col"
-      >
+      <div class="w-full relative rounded-2xl overflow-hidden flex flex-col">
         <img
           src="https://images.unsplash.com/photo-1716968921433-835ec227f847?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Welcome Aboard"
           class="w-full h-1/2 object-cover"
         />
         <div
-          class="bg-gray-900 bg-opacity-30 p-6 flex-grow rounded-b-2xl flex flex-col justify-between"
+          class="bg-secondary-color p-6 flex-grow rounded-b-2xl flex flex-col justify-between"
         >
           <div>
             <h1 class="text-2xl font-semibold text-white">SCHOOL OF ICT</h1>
