1c9d4fb617f2746ef59aface36324fc1fdc453e3
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-07-23 13:49:12 +0300

    update content

--- app/src/components/soiBusiness.vue
+++ app/src/components/soiBusiness.vue
@@ -84,30 +84,13 @@
                 Computer foundational skills
               </h1>
               <p class="text-white py-3">
-                Welcome to the Computer Foundation Skills course offered by the
-                School of ICT. This course is designed to provide students with
-                a solid grounding in essential computer skills, ensuring they
-                have the knowledge and capabilities required to succeed in
-                today’s digital world. Whether you are a beginner or looking to
-                rzskills needed for effective computer use.
-                <br /><span class="font-semibold text-xl">Course Duration</span
-                ><br />
-                This is a 8-week course, with classes held daily. Each session
-                will last approximately 2 hours, with additional time allocated
-                for lab work and project collaboration.
-                <br /><span class="font-semibold text-xl">Enrollment</span
-                ><br />
-                Enrollment is open to all students with a basic understanding of
-                high school-level mathematics. No prior computer knowledge is
-                required. Interested individuals can sign up through the School
-                of ICT’s online portal.
+                This course is designed to provide students with a solid
+                grounding in essential computer skills, ensuring they have the
+                knowledge and capabilities required to succeed in today's
+                digital world. Whether you are a beginner or looking to skills
+                needed for effective computer use.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
 
@@ -131,30 +114,9 @@
                 foundation in Information and Communication Technology. Ideal
                 for beginners aiming to enter the ICT field or professionals
                 seeking to bolster their skills, the program covers a diverse
-                array of fundamental topics. Graduates emerge well-equipped to
-                meet the challenges of today's technology-driven workplace,
-                ready to apply their knowledge effectively in various ICT roles.
-
-                <br /><span class="font-semibold text-xl"
-                  >Admission Requirements</span
-                ><br />
-                Basic knowledge of high school-level mathematics. No prior ICT
-                experience required, making it accessible to beginners.
-                Applicants should have a keen interest in technology and
-                problem-solving.
-
-                <br /><span class="font-semibold text-xl">How to Apply</span
-                ><br />
-                Interested candidates can apply through the School of ICT’s
-                online application portal. For more information, visit our
-                admissions page or contact our admissions office.
+                array of fundamental topics.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
         <!-- card 3 -->
@@ -172,35 +134,15 @@
                 Diploma in ICT
               </h1>
               <p class="text-white py-3">
-                The Diploma in ICT is a comprehensive program designed to equip
-                students with advanced skills and knowledge in Information and
-                Communication Technology. This diploma bridges the gap between
-                foundational understanding and specialized expertise, preparing
-                graduates for a wide range of professional roles in the ICT
-                sector. With a balanced mix of theoretical concepts and
+                A comprehensive program designed to equip students with advanced
+                skills and knowledge in ICT. This diploma bridges the gap
+                between foundational understanding and specialized expertise,
+                preparing graduates for a wide range of professional roles in
+                the ICT sector. With a balanced mix of theoretical concepts and
                 practical applications, this program is ideal for those aspiring
                 to become proficient ICT professionals.
-
-                <br /><span class="font-semibold text-xl"
-                  >Duration and Schedule</span
-                ><br />
-                The Diploma in ICT is a 1-year full-time program or a 2-year
-                part-time program. Classes are held four times a week, with each
-                session lasting 3 hours. Additional lab sessions and project
-                work will be scheduled as required.
-
-                <br /><span class="font-semibold text-xl">How to Apply </span
-                ><br />
-                Interested candidates can apply through the School of ICT’s
-                online application portal. For more information, visit our
-                admissions page or contact our admissions office.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
         <!-- card 4 -->
@@ -222,25 +164,9 @@
                 cultivate creative and technical skills in the field of visual
                 communication. This program provides students with a
                 comprehensive understanding of graphic design principles, tools,
-                and techniques. Through a combination of theoretical study and
-                hands-on practice, students will learn to create visually
-                compelling designs that effectively communicate messages across
-                various media. Duration is what they should tell us how long and
-                what is the weight of certification is it dip or cert or degree
-                they need to specify
-
-                <br /><span class="font-semibold text-xl">How to Apply</span
-                ><br />
-                Interested candidates can apply through the School of ICT’s
-                online application portal. For more information, visit our
-                admissions page or contact our admissions office.
+                and techniques.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
         <!-- card 5 -->
@@ -263,34 +189,9 @@
                 intelligence and machine learning technologies. It covers
                 essential theories, tools, and practical applications, preparing
                 students for dynamic roles in the rapidly evolving tech
-                industry. Graduates emerge with the skills to design, develop,
-                and implement AI and machine learning solutions across diverse
-                sectors, making them highly sought-after professionals in the
-                field.
-
-                <br /><span class="font-semibold text-xl"
-                  >Teaching Methods </span
-                ><br />
-                Lectures and Seminars: Comprehensive coverage of theoretical
-                concepts. Hands-on Labs: Practical sessions to apply AI and
-                machine learning techniques. Group Projects: Collaborative
-                projects to develop real-world AI solutions. Case Studies:
-                Analysis of successful AI implementations. Assessments: Quizzes,
-                assignments, and a final capstone project to evaluate
-                proficiency.
-
-                <br /><span class="font-semibold text-xl">How to Apply</span
-                ><br />
-                Interested candidates can apply through the School of ICT’s
-                online application portal. For more information, visit our
-                admissions page or contact our admissions office.
+                industry.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
         <!-- card 6 -->
@@ -315,23 +216,9 @@
                 science, analytics, and research, the curriculum provides a
                 comprehensive understanding of statistical methods and their
                 practical application using widely adopted languages such as R
-                and Python. Graduates are equipped to employ advanced
-                statistical techniques to derive insights and solutions from
-                complex datasets, making them well-prepared for diverse roles in
-                analytical fields. ask for the duration of this course.
-
-                <br /><span class="font-semibold text-xl">How to Apply</span
-                ><br />
-                Interested candidates can apply through the School of ICT’s
-                online application portal. For more information, visit our
-                admissions page or contact our admissions office.
+                and Python.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
         <div class="w-full relative rounded-2xl overflow-hidden flex flex-col">
@@ -351,26 +238,10 @@
                 The Diploma in Data Management and Analytics is an intensive and
                 comprehensive program tailored to equip students with the
                 essential skills and knowledge in data management, analysis, and
-                business intelligence. Ideal for aspiring data analysts and
-                managers, the curriculum spans the entire data lifecycle—from
-                collection and storage to thorough analysis and impactful
-                visualization. Graduates emerge well-prepared to lead
-                data-driven decision-making processes across diverse industries,
-                leveraging their expertise to navigate and harness the power of
-                data effectively..
-
-                <br /><span class="font-semibold text-xl">How to Apply</span
-                ><br />
-                Interested candidates can apply through the School of ICT’s
-                online application portal. For more information, visit our
-                admissions page or contact our admissions office.
+                business intelligence.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
+            
           </div>
         </div>
         <div class="w-full relative rounded-2xl overflow-hidden flex flex-col">
@@ -390,25 +261,9 @@
                 At Our School of ICT, the Website Development program is
                 meticulously crafted to empower students with the skills and
                 knowledge essential for building modern, responsive, and dynamic
-                websites. This comprehensive curriculum spans a broad spectrum
-                of topics, encompassing HTML, CSS, JavaScript, front-end
-                frameworks, back-end development, and database management. Our
-                students gain proficiency in creating visually appealing and
-                user-friendly websites that operate seamlessly across diverse
-                devices and browsers.
-
-                <br /><span class="font-semibold text-xl">How to Apply</span
-                ><br />
-                Interested candidates can apply through the School of ICT’s
-                online application portal. For more information, visit our
-                admissions page or contact our admissions office.
+                websites.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
         <div class="w-full relative rounded-2xl overflow-hidden flex flex-col">
@@ -427,22 +282,9 @@
               <p class="text-white py-3">
                 Our Software Development program at the School of ICT is crafted
                 to equip students with a solid foundation in creating, testing,
-                and maintaining software applications. We emphasize hands-on
-                learning through projects that replicate real-world scenarios,
-                ensuring our students gain practical skills highly valued in the
-                tech industry. Throughout the program, students delve into
-                essential programming languages such as Python, Java, and C++,
-                alongside mastering modern software development methodologies
-                like Agile and DevOps. By graduation, our students are
-                proficient in developing scalable, efficient, and secure
-                software solutions.
+                and maintaining software applications.
               </p>
             </div>
-            <router-link
-              to="#"
-              class="text-black font-semibold hover:cursor-pointer px-6 py-2 bg-white rounded-3xl self-start"
-              >Read More</router-link
-            >
           </div>
         </div>
       </div>
