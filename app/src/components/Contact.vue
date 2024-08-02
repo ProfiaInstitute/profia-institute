0ab6935c6f7e75bd97fe8e44244ee470b3ba9fda
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-08-03 02:25:54 +0300

    message

--- app/src/components/Contact.vue
+++ app/src/components/Contact.vue
@@ -4,22 +4,23 @@
       class="w-full max-w-6xl flex justify-end px-8 mx-auto bg-secondary-color text-black text-center text-3xl font-semibold rounded-lg py-4 mb-10"
     >
       <h2 class="text-white">Reach out to us</h2>
-      
     </div>
     <div class="flex justify-start ml-20 -mt- items-center -mt-16">
       <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
-        <div class="bg-white p-4 rounded-lg">
+        <div class="bg-white p-4 rounded-lg space-y-4 text-lg">
           <p>Profia Institute</p>
-          <p>P.O Box 1234</p>
-          <p>Kisumu</p>
+          <p>P.O Box 323-40300</p>
+          <p>Homa Bay</p>
         </div>
       </div>
     </div>
     <div
-      class="flex flex-col items-center sm:items-start pl-4 sm:pl-48 justify-center mt-7 text-center sm:text-left"
+      class="flex flex-col items-center sm:items-start pl-4 sm:pl-48 justify-center mt-7 text-center sm:text-left space-y-3 text-lg"
     >
-      <p>Email: Profia@gmail.com</p>
-      <p>Phone Number: 0712345345</p>
+      <p><span class="font-semibold">Email:</span> profiainstitute@gmail.com</p>
+      <p>
+        <span class="font-semibold">Phone Number:</span> 0710221122, 0793977991
+      </p>
     </div>
 
     <div class="mx-2 sm:mx-12 mt-20 gap-8 flex">
@@ -42,11 +43,13 @@
           placeholder="Number:"
         />
         <textarea
-          class="text-lg sm:text-xl p-2 sm:p-3 h-32 sm:h-44 border-[1px] border-gray-400" 
+          class="text-lg sm:text-xl p-2 sm:p-3 h-32 sm:h-44 border-[1px] border-gray-400"
           name="comment"
           placeholder="Enter text here..."
         ></textarea>
-        <button class="text-sm sm:text-lg text-white p-2 sm:p-3 bg-primary-color w-32 sm:w-44 rounded-xl">
+        <button
+          class="text-sm sm:text-lg text-white p-2 sm:p-3 bg-primary-color w-32 sm:w-44 rounded-xl"
+        >
           Send message
         </button>
       </form>
@@ -58,7 +61,7 @@
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15958.577394944372!2d34.4575996!3d-0.5350592!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19d4d58bffe5db35%3A0x403b4b18e49e44b9!2sProfia%20Institute%20of%20Professionals!5e0!3m2!1sen!2ske!4v1719481692840!5m2!1sen!2ske"
           width="650"
           height="500"
-          style="border: 0;"
+          style="border: 0"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
