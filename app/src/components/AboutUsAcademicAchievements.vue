6033857d1a6cea58b884345053698678a6a34df5
meccah <georgekimiti79@gmail.com>
2024-08-09 23:02:33 +0300

    message

--- app/src/components/AboutUsAcademicAchievements.vue
+++ app/src/components/AboutUsAcademicAchievements.vue
@@ -1,45 +1,106 @@
 <template>
- <div class="text-gray-800 bg-primary-900">
+  <div class="text-gray-800 bg-primary-900">
     <section class="max-w-7xl mx-auto py-5">
-        <h1 class="text-secondary-color text-center text-3xl md:text-5xl font-bold sm:mb-16 mb-28 pb-6">
-            Academic Achievements
+      <h1
+        class="text-secondary-color text-center text-3xl md:text-5xl font-bold sm:mb-16 mb-28 pb-6"
+      >
+        Academic Achievements
+      </h1>
+      <div
+        class="relative bg-slate-200 sm:rounded-tr-3xl h-auto lg:h-96 sm:w-[90%] lg:w-[800px] mx-auto pb-4 px-4 md:px-12"
+      >
+        <h1 class="text-center font-bold text-2xl py-2 text-gray-600">
+          Laurina Cebrels Dracula
         </h1>
-        <div class="relative bg-slate-200 sm:rounded-tr-3xl h-auto lg:h-96 sm:w-[90%] lg:w-[800px] mx-auto pb-4 px-4 md:px-12">
-            <h1 class="text-center font-bold text-2xl py-2 text-gray-600">
-                Laurina Cebrels Dracula
-            </h1>
-            <p class="px-5 md:px-20 md:text-center py-5 text-lg">
-                A degree in Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Nostrum, eaque eligendi nemo voluptatem libero corporis aperiam, error
-                deserunt quia iusto velit sed. Nihil, expedita delectus.
-            </p>
-            <div class="absolute h-56 w-52 opacity-30 sm:opacity-100 bottom-12 left-20 sm:-bottom-7 sm:-left-40 md:-left-20 mb-20">
-                <img src="../assets/images/alexander-krivitskiy.avif" class="rounded-2xl" alt="" />
-            </div>
+        <p class="px-5 md:px-20 md:text-center py-5 text-lg">
+          A degree in Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nostrum, eaque eligendi nemo voluptatem libero corporis aperiam, error
+          deserunt quia iusto velit sed. Nihil, expedita delectus.
+        </p>
+        <div
+          class="absolute h-56 w-52 opacity-30 sm:opacity-100 bottom-12 left-20 sm:-bottom-7 sm:-left-40 md:-left-20 mb-20"
+        >
+          <img
+            src="../assets/images/alexander-krivitskiy.avif"
+            class="rounded-2xl"
+            alt=""
+          />
         </div>
+      </div>
 
-        <div class="relative bg-slate-200 h-auto sm:h-[400px] lg:h-96 sm:w-[90%] lg:w-[800px] mx-auto mt-20 sm:rounded-bl-3xl pb-6 px-4 md:px-8">
-            <h1 class="text-center font-bold text-2xl py-2 text-gray-600">
-                Veleria Van Helsing
-            </h1>
-            <p class="px-5 md:px-20 py-5 text-lg md:text-center">
-                A masters in Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Nesciunt, numquam eius dolorum, ut voluptas tenetur ipsum est enim
-                culpa tempora nobis! Corporis mollitia ipsum nihil.
-            </p>
-            <div class="absolute h-56 opacity-30 sm:opacity-100 w-52 top-16 right-20 sm:-top-7 sm:-right-40 md:-right-20">
-                <img src="../assets/images/andrey-k.avif" class="rounded-2xl" alt="" />
-            </div>
+      <div
+        class="relative bg-slate-200 h-auto sm:h-[400px] lg:h-96 sm:w-[90%] lg:w-[800px] mx-auto mt-20 sm:rounded-bl-3xl pb-6 px-4 md:px-8"
+      >
+        <h1 class="text-center font-bold text-2xl py-2 text-gray-600">
+          Veleria Van Helsing
+        </h1>
+        <p class="px-5 md:px-20 py-5 text-lg md:text-center">
+          A masters in Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nesciunt, numquam eius dolorum, ut voluptas tenetur ipsum est enim
+          culpa tempora nobis! Corporis mollitia ipsum nihil.
+        </p>
+        <div
+          class="absolute h-56 opacity-30 sm:opacity-100 w-52 top-16 right-20 sm:-top-7 sm:-right-40 md:-right-20"
+        >
+          <img
+            src="../assets/images/andrey-k.avif"
+            class="rounded-2xl"
+            alt=""
+          />
         </div>
+      </div>
       <div class="text-center mx-auto space-y-5 py-10 mt-24 lg:mt-0 md:mt-0">
         <h2 class="font-bold text-3xl md:text-5xl text-secondary-color">
           Our History
         </h2>
         <p class="max-w-2xl mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo
-          dolores voluptates at natus ad dolorem autem quis! Eaque accusantium
-          perspiciatis ipsam dolor voluptatem, assumenda nisi qui eius
-          voluptatum neque molestias aspernatur, ad quibusdam? Harum?
+          Cultivating Potential in Homa Bay , Nyanza Region and beyond. Profia
+          Institution, established in October 2018, is a testament to the power
+          of virtue-driven education in Homa Bay County. With a resolute focus
+          on fostering sustainable growth within the community, Profia offers a
+          diverse range of developmental courses that empower individuals and
+          equip them with the skills necessary to thrive in the ever-evolving
+          job market.
+        </p>
+
+        <h3 class="text-2xl text-secondary-color">From Humble Beginnings to Flourishing Institution:</h3>
+        <p class="max-w-2xl mx-auto">
+          Profia's journey began in 2018 as a business school, catering to the
+          growing demand for business acumen within the region. Recognizing the
+          need for a well-rounded educational experience, Profia has steadily
+          expanded its course offerings to encompass a comprehensive curriculum.
+          Today, students can pursue certifications from esteemed organisations
+          like KASNEB (Kenya Accountants and Secretaries National Examinations
+          Board), NITA (National Industrial Training Authority), and TVET
+          (Technical and Vocational Education and Training).
+        </p>
+        <h3 class="text-2xl text-secondary-color">A Spectrum of Opportunity:</h3>
+        <p class="max-w-2xl mx-auto text-left">
+          Profia's course catalogue boasts a variety of options, catering to a
+          wide range of aspirations. Individuals seeking careers in finance can
+          delve into the intricacies of accounting through the CPA (Certified
+          Public Accountant) program. For those with a passion for technology,
+          Profia's ICT (Information and Communication Technology) courses
+          provide the necessary foundation for success in the digital age.
+          Business courses equip students with the knowledge and skills to
+          navigate the dynamic world of commerce, while programs in hospitality,
+          cosmetology, and plumbing cater to specialised vocations that are
+          crucial to the community's infrastructure.
+        </p>
+        <h3 class="text-2xl text-secondary-color">Investing in the Future:</h3>
+        <p class="max-w-2xl mx-auto text-left">
+          Profia's commitment to excellence extends beyond its curriculum. The
+          institution boasts a state-of-the-art ICT lab, ensuring students have
+          access to the latest technology and fostering a dynamic learning
+          environment. At the heart of Profia lies its dedicated faculty – a
+          team of 15 strong tutors who share their expertise and passion with a
+          current student population of 95. This close-knit community fosters
+          personalised attention and a supportive learning atmosphere. Profia
+          Institution's story is one of continuous growth and unwavering
+          dedication to its students and the community. As Homa Bay County
+          marches forward, Profia stands as a beacon of opportunity, shaping
+          futures and empowering individuals to become the architects of their
+          own success.
         </p>
       </div>
     </section>
