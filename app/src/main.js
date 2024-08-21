7a0797d096997c40234cdeda10ccd7bb190b1f21
joe052 <joekaranjasenior52@gmail.com>
2024-08-21 23:44:19 +0300

    message

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
 
@@ -7,13 +6,7 @@ import App from "./App.vue";
 // import "./style.css";
 import "./assets/tailwind.css"; //tailwind
 
-// setup fake backend
-// import { fakeBackend } from "./helpers";
-// fakeBackend();
-
 const app = createApp(App);
-const pinia = createPinia();
 
-app.use(pinia);
 app.use(router);
 app.mount("#app");
